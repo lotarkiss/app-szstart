d05017f6ea5a6b0e33a38d26dc4c40b6f9791a31
lotarkiss <144259980+lotarkiss@users.noreply.github.com>
2026-08-01 16:52:47 +0200

    Log dialog / frame

---
+++ frames/frmServerLog.pas
@@ -0,0 +1,56 @@
+unit frmServerLog;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, ExtCtrls, StdCtrls, Buttons, uServer;
+
+type
+
+  { TframeServerLog }
+
+  TframeServerLog = class(TFrame)
+    btnCommand: TButton;
+    cbCommand: TComboBox;
+    lbCommand: TLabel;
+    lbPlayerTitle: TLabel;
+    lbLog: TLabel;
+    lbTitle: TLabel;
+    lbPlayers: TListBox;
+    mmLog: TMemo;
+    pnTop: TPanel;
+    btnOpen: TSpeedButton;
+    pnRight: TPanel;
+    pnBottom: TPanel;
+    pnClient: TPanel;
+    spRight: TSplitter;
+    procedure btnOpenClick(Sender: TObject);
+  private
+  public              
+    procedure UpdateFrame(const Server: TCustomServer);
+  end;
+
+implementation
+
+uses LCLIntf;
+
+{$R *.lfm}
+
+{ TframeServerLog }
+
+procedure TframeServerLog.btnOpenClick(Sender: TObject);
+begin
+  OpenDocument((Sender as TSpeedButton).Hint);
+end;
+
+procedure TframeServerLog.UpdateFrame(const Server: TCustomServer);
+begin
+  lbTitle.Caption := Server.Server.Name;
+  btnOpen.Hint := ExcludeTrailingPathDelimiter(Server.Server.Path);
+  mmLog.Lines.Assign(Server.Log);
+end;
+
+end.
+
